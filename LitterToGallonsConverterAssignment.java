5c4d0611f692d6cc423d2dd46d31c6ddcaaf3fb9
Moejabbie <129770937+Moejabbie@users.noreply.github.com>
2025-05-06 22:24:22 -0700

    Add files via upload
    Reattempted

--- LitterToGallonsConverterAssignment.java
+++ LitterToGallonsConverterAssignment.java
@@ -2,22 +2,34 @@ public class LitterToGallonsConverterAssignment {
 
     public static void main(String[] args) {
 
-        double liters = 3.79;
-        double gallons = liters * 0.264172;
-
+        double liters, gallons;
+        int counter = 0;
         for (liters = 1; liters <= 100; liters = liters + 4) {
             gallons = liters * 0.264172;
 
+            if (liters == 1) {
+                System.out.printf("%.2f liter = %.2f galons%n", liters, gallons);
+            } else {
+                System.out.printf("%.2f liters = %.2f galons%n", liters, gallons);
+            }
 
-            System.out.printf("%.2f liters = %.2f galons%n", liters,gallons);
 
 
+            counter ++;
+            if (counter == 5) {
+                System.out.println();
+                counter = 0;
+
+            }
+
+            }
+        System.out.println(" We are done!!!");
+        counter = 0;
         }
-
-
-        }
-
     }
+
+
+
 
 
 
